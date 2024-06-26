0152cfb8cbf439fdbe1904aad41618b20a829ce6
Karol Sułek <Karol.sul3k@gmail.com>
2024-06-26 09:29:39 +0200

    Correct iformation window

--- Encrypt-omat/AppDelegate.cs
+++ Encrypt-omat/AppDelegate.cs
@@ -27,9 +27,17 @@ namespace Encryptomat
         {
             var alert = new NSAlert()
             {
+
                 AlertStyle = NSAlertStyle.Informational,
                 MessageText = "Witamy w naszej aplikacji!",
-                InformativeText = "To jest informacja wyświetlana przy starcie aplikacji."
+                InformativeText = "Aplikacja ma na celu szyfrowanie i odszyfrowywanie zdań, \ntekstów oraz haseł.\n\n " +
+                "Oto krótka instrukcja użytkowania:\n\n" +
+
+                "1.Na górze znajduje się suwak, którym wybieramy, czy chcemy zaszyfrować, \nczy odszyfrować tekst lub ciąg liczb.\n" +
+                "2.Poniżej znajduje się pole, w które wpisujemy tekst oraz, \n w zależności od metody szyfrowania, klucz.\n " +
+                "**Uwaga:** Nie każda metoda szyfrowania wymaga klucza.\n" +
+                "3.Na samym dole wyświetlany jest wynik - zaszyfrowany tekst. \nPoniżej znajduje się przycisk \"Kopiuj\", który umożliwia łatwe skopiowanie zaszyfrowanego tekstu.\n\n" +
+                "**Ważna informacja:** Przycisk \"Kopiuj\" kopiuje tylko zaszyfrowany tekst. \nKlucz musimy zapamiętać osobno, \n ponieważ bez niego nie będziemy w stanie odszyfrować tekstu. "
             };
             alert.AddButton("OK");
             alert.RunModal();
